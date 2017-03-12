9191d8283b717769e74425a3d072cad21f7c3053
Lenis <lenis084@hotmail.com>
2017-03-12 14:20:14 -0300

    Se actualizó class.mydbcon
    se actualizo la conexion remota a base de datos

--- includes/class.mydbcon.inc.php
+++ includes/class.mydbcon.inc.php
@@ -7,7 +7,7 @@ Date : 2006.11.27  Time : 1:00 pm
 
 class classmydbcon{
 	# Development servidor Hostinger
-	var $dbhost="mysql.hostinger.es";
+	var $dbhost="31.220.104.1";
 	var $db="u186334426_cdemp";
 	var $uname="u186334426_cdemp";
 	var $pwd="cdemp2016";
@@ -21,60 +21,60 @@ class classmydbcon{
 	// var $link="";
 	// var $error="";
 
-	
+
 	# class constructor
 	function __construct(){
-		$this->set_conn_string($this->dbhost,$this->db,$this->uname,$this->pwd);		
+		$this->set_conn_string($this->dbhost,$this->db,$this->uname,$this->pwd);
 	} // function classmydbcon
 
 	function set_conn_string($host,$db,$uname,$pwd,$conn_type='normal'){
-		if ($this->link = mysqli_connect($host,$uname,$pwd,$db)){	
-							
-			#return $this->link;				
+		if ($this->link = mysqli_connect($host,$uname,$pwd,$db)){
+
+			#return $this->link;
 			return true;
-							
+
 		}else{
-			
+
 			return false;
-		} // if		
+		} // if
 	} // function set_conn_string
 
 	function get_resource_link(){
-		
+
 		return $this->link;
-		
+
 	} // function get_conn_string
-	
+
 	function get_result_set($query){
-	
+
 		if (!$result = mysqli_query($this->link,$query)){
 			$this->error = mysqli_error($this->link);
 			return false;
 		}else{
 			return $result;
 		} // if !result
-	
+
 	} // function get_result_set
-	
+
 	function set_query($query){
-		
+
 		if (!mysqli_query($this->link,$query)){
 			echo "Error in query $query<br>" . mysqli_error($this->link);
 			return false;
 		}else{
 			return true;
 		} // if !result
-		
+
 	} // function set_query
-	
+
 	function get_error(){
 		return $this->error;
 	} // if
-	
+
 	function get_last_id(){
 		return mysqli_insert_id($this->link);
 	} // function
-	
+
 	function get_affected_rows(){
 		if (is_resource($this->link)){
 			return mysqli_affected_rows($this->link);
