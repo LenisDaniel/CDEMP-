6354e2c0b77f15aff7ae51a1c1dcd00b6e33f914
Lenis <lenis084@hotmail.com>
2017-05-21 07:36:20 -0300

    Clase teacher hereda de Users
    Ya la clase teacher tiene herencia de users.

--- includes/manage_teachers.inc.php
+++ includes/manage_teachers.inc.php
@@ -1,41 +1,41 @@
 <?php
+error_reporting(E_ALL);
 require_once("class.teachers.inc.php");
+
 $objteachers = new Teachers();
 
-
-
-    $objtemplate->set_content("teachers_td", $objteachers->get_all_teachers());
+    $objtemplate->set_content("teachers_td", $objteachers->get_all_users(3, 'manage_teachers'));
 
     if(isset($_GET['edit'])){
 
         $idx = base64_decode($_GET['edit']);
-        $objteachers->get_teacher($idx);
+        $objteachers->get_user($idx);
         $objtemplate->set_content("form_action", "display_page.php?tpl=manage_teachers&cid=".base64_encode($idx));
         $objtemplate->set_content("form_title", "Update Teacher Info");
         $objtemplate->set_content("send_button", "Update");
 
         //aqui llenamos el formulario con la info de la base de datos
-        $objtemplate->set_content("first_name", $objteachers->get_teacher_info('first_name'));
-        $objtemplate->set_content("last_name", $objteachers->get_teacher_info('last_name'));
-        $objtemplate->set_content("second_surname", $objteachers->get_teacher_info('second_surname'));
-        $objtemplate->set_content("email", $objteachers->get_teacher_info('email'));
-        $objtemplate->set_content("password", $objteachers->get_teacher_info('password'));
-        $objtemplate->set_content("address1", $objteachers->get_teacher_info('address1'));
-        $objtemplate->set_content("address2", $objteachers->get_teacher_info('address2'));
-        $objtemplate->set_content("cities_dd", $objteachers->get_cities($objteachers->get_teacher_info('city')));
-        $objtemplate->set_content("states_dd", $objteachers->get_state($objteachers->get_teacher_info('state')));
-        $objtemplate->set_content("phone_1", $objteachers->get_teacher_info('phone_1'));
-        $objtemplate->set_content("phone_2", $objteachers->get_teacher_info('phone_2'));
-        $objtemplate->set_content("zipcode", $objteachers->get_teacher_info('zipcode'));
+        $objtemplate->set_content("first_name", $objteachers->get_user_info('first_name'));
+        $objtemplate->set_content("last_name", $objteachers->get_user_info('last_name'));
+        $objtemplate->set_content("second_surname", $objteachers->get_user_info('second_surname'));
+        $objtemplate->set_content("email", $objteachers->get_user_info('email'));
+        $objtemplate->set_content("password", $objteachers->get_user_info('password'));
+        $objtemplate->set_content("address1", $objteachers->get_user_info('address1'));
+        $objtemplate->set_content("address2", $objteachers->get_user_info('address2'));
+        $objtemplate->set_content("cities_dd", $objteachers->get_cities($objteachers->get_user_info('city')));
+        $objtemplate->set_content("states_dd", $objteachers->get_state($objteachers->get_user_info('state')));
+        $objtemplate->set_content("phone_1", $objteachers->get_user_info('phone_1'));
+        $objtemplate->set_content("phone_2", $objteachers->get_user_info('phone_2'));
+        $objtemplate->set_content("zipcode", $objteachers->get_user_info('zipcode'));
 
-        if($objteachers->get_teacher_info('active') == 1){
+        if($objteachers->get_user_info('active') == 1){
             $objtemplate->set_content("option1", "checked");
         }else{
             $objtemplate->set_content("option2", "checked");
         }
 
     }else{
-        //exit("entre 2");
+
         $objtemplate->set_content("form_action", "display_page.php?tpl=manage_teachers&cid=".base64_encode(0));
         $objtemplate->set_content("cities_dd", $objteachers->get_cities(0));
         $objtemplate->set_content("states_dd", $objteachers->get_state(0));
@@ -63,9 +63,6 @@ $objteachers = new Teachers();
 
     if(isset($_GET['cid'])){
 
-//        print_r($_POST);
-//        exit;
-
         $id = base64_decode($_GET['cid']);
         $first_name = $_POST['first_name'];
         $last_name = $_POST['last_name'];
@@ -82,13 +79,8 @@ $objteachers = new Teachers();
         $active = $_POST['active'];
         $role_idx = 3;
 
-
-
-        $objteachers->manage_teacher_info($id, $first_name, $last_name, $second_surname, $password, $email, $address1, $address2, $phone_1, $phone_2, $cities_dd, $states_dd, $zipcode, $active, $role_idx);
+        $objteachers->manage_user_info('manage_teachers', $id, $first_name, $last_name, $second_surname, $password, $email, $address1, $address2, $phone_1, $phone_2, $cities_dd, $states_dd, $zipcode, $active, $role_idx);
 
     }
-
-
-
 
 ?>
