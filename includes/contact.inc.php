ae889454c219d9e04b626f217aff4efe801be48e
Lenis <lenis084@hotmail.com>
2017-05-25 11:43:46 -0300

    Contact form
    Se cambio el código para usar la clase phpmailer

--- includes/contact.inc.php
+++ includes/contact.inc.php
@@ -2,76 +2,65 @@
 
 /**
  * Created by PhpStorm.
- * User: Jonathan Flores
+ * User: Jonathan Flores and Lenis Rivera
  * Date: 5/16/2017
  * Time: 9:31 PM
  */
-// variables
-$_name_error = $email_error = $subject_error = "";
-$name = $email = $message = $subject = $success = "";
 
 
-//if(isset($_POST['name'])){
-//    echo "<pre>";
-//    print_r($_POST);
-//    echo "</pre>";
-//    exit;
-//}
+require_once("includes/class.phpmailer.php");
+require_once("includes/class.smtp.php");
+require("includes/PHPMailerAutoload.php");
 
 
-// validation rules
-if($_SERVER["REQUEST METHOD"] == "POST") {
-    if(empty($_POST["name"])) {
-        $name_error = "Name is required";
-    }else{
-        $name = test_input($_POST["name"]);
-        if(!preg_match("/^[a-zA-Z]*$/", $name)){
-            $name_error = "Only letters and white space allowed";
-        }
-    }
+$email = $_POST['email'];
+$subject = $_POST['subject'];
+$name = $_POST['name'];
+$message = $_POST['message'];
+
+
+
+
+
+$mail = new PHPMailer();
+//$mail->SMTPDebug = 1;
+$mail->isSMTP();
+$mail->CharSet = 'UTF-8';
+$mail->Host = 'smtp.gmail.com';
+$mail->SMTPAuth = true;
+$mail->Port = 25;
+$mail->Username = 'lenis.daniel@gmail.com';
+$mail->Password = 'playaysol2584';
+$mail->SMTPSecure = 'ssl';
+
+
+$mail->setFrom('info@lenisrivera.info', 'Prueba ICY-ATP');
+
+$mail->addAddress($email);
+
+
+//$mail->addAddress('lenis.daniel@gmail.com');
+$mail->addReplyTo('info@lenisrivera.info', 'Information');
+
+//$mail->addAttachment('pdfs/filename_'.$id.'.pdf', 'plan_transicion.pdf'); // Add attachments
+
+$mail->isHTML(true);
+
+$mail->Subject = $subject;
+$mail->Body    = $message;
+$mail->AltBody = $message;
+
+if(!$mail->send()) {
+    echo 'Message could not be sent.';
+    echo 'Mailer Error: ' . $mail->ErrorInfo;
+} else {
+    header("location: complete.php");
+    //echo 'Message has been sent';
 }
-// require fields
-if(empty($_POST["email"])) {
-    $email_error = "Email is required";
-}else{
-    $email = test_input($_POST["email"]);
 
-    if(!filter_var($email,FILTER_VALIDATE_EMAIL)){
-        $email_error = "Invalid email format";
-    }
-}
-// if message variable is not with the correct parameters
-if(empty($_POST["message"])){
-    $message ="";
-}else{
-    $message = test_input($_POST["message"]);
-}
-if ($name_error == '' and $email_error == '' and $subject_error == '') {
-    $message_body = '';
-    unset($_POST['submit']);
-    foreach ($_POST as $key => $value) {
-        $message_body .= "$key: $value\n";
-    }
-// sender variables
-    $to = $_POST['email'];
-    $subject = $_POST['subject'];
-// email send
-    if (mail($to, $subject, $message)) {
-        $success = "Message sent, thank you for contacting us";
-        echo $success;
-        $name = $email = $subject = $message = "";
-    }else{
-        echo "Fallo";
-    }
-// function testing
-    function test_input($data)
-    {
-        $data = trim($data);
-        $data = stripssplashes($data);
-        $data = htmlspecialchars($data);
-        return $data;
-    }
-}
+
+
+
 
 
 
