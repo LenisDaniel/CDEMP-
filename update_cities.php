5a7ec5ac6523640b67d9f897cb05f0da81f6962f
Lenis <lenis084@hotmail.com>
2017-07-12 08:18:48 -0300

    update-cities
    update cities para arreglar los acentos de los pueblos en cdemp

---
+++ update_cities.php
@@ -0,0 +1,99 @@
+<?php
+require_once ("includes/class.mydbcon.inc.php");
+$objmydbcon = new classmydbcon();
+
+$cities = array(
+    'Adjuntas',
+    'Aguada',
+    'Aguadilla',
+    'Aguas Buenas',
+    'Aibonito',
+    'Arecibo',
+    'Arroyo',
+    'Añasco',
+    'Barceloneta',
+    'Barranquitas',
+    'Bayamón',
+    'Cabo Rojo',
+    'Caguas',
+    'Camuy',
+    'Canóvanas',
+    'Carolina',
+    'Cataño',
+    'Cayey',
+    'Ceiba',
+    'Ciales',
+    'Cidra',
+    'Coamo',
+    'Comerío',
+    'Corozal',
+    'Culebra',
+    'Dorado',
+    'Fajardo',
+    'Florida',
+    'Guayama',
+    'Guayanilla',
+    'Guaynabo',
+    'Gurabo',
+    'Guánica',
+    'Hatillo',
+    'Hormigueros',
+    'Humacao',
+    'Isabela',
+    'Jayuya',
+    'Juana Díaz',
+    'Juncos',
+    'Lajas',
+    'Lares',
+    'Las Marías',
+    'Las Piedras',
+    'Loiza',
+    'Luquillo',
+    'Manatí',
+    'Maricao',
+    'Maunabo',
+    'Mayagüez',
+    'Moca',
+    'Morovis',
+    'Naguabo',
+    'Naranjito',
+    'Orocovis',
+    'Patillas',
+    'Peñuelas',
+    'Ponce',
+    'Quebradillas',
+    'Rincón',
+    'Rio Grande',
+    'Sabana Grande',
+    'Salinas',
+    'San Germán',
+    'San Juan',
+    'San Lorenzo',
+    'San Sebastián',
+    'Santa Isabel',
+    'Toa Alta',
+    'Toa Baja',
+    'Trujillo Alto',
+    'Utuado',
+    'Vega Alta',
+    'Vega Baja',
+    'Vieques',
+    'Villalba',
+    'Yabucoa',
+    'Yauco'
+);
+$j = 0;
+
+for($i = 1; $i < 78; $i++){
+
+    $city = $cities[$j];
+    echo $city;
+    exit;
+    $sqlupdate = "UPDATE master_cities SET name = '$city' WHERE city_id = $i";
+    if($objmydbcon->set_query($sqlupdate)){
+        echo $i . "insertado";
+    }else{
+        return false;
+    }
+    $j++;
+}
